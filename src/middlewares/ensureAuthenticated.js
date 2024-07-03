75dfd91ef53ba15c70e6606c49abf01c772c3dcb
jelly <jamesla200210@gmail.com>
2024-07-03 13:47:41 +1200

    restart with Nextjs

--- src/middlewares/ensureAuthenticated.js
+++
@@ -1,9 +0,0 @@
-function ensureAuthenticated(req, res, next) {
-    if (req.session && req.session.user) {
-        return next();
-    } else {
-        res.status(401).send('Please log in first to use these operations.');
-    }
-}
-
-module.exports = ensureAuthenticated;
